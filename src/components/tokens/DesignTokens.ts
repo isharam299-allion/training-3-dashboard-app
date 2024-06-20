9d720ba11e6f23c4130a63247f7eff29ea8439a9
Ishara Madushani <isharam@alliontechnologies.com>
2024-06-20 13:31:47 +0530

    Add font and design tokens

---
+++ src/components/tokens/DesignTokens.ts
@@ -0,0 +1,49 @@
+export const tokens = {
+    colors : {
+        // Background Colors
+        bgColorOrange :'#EB763C',
+        bgColorGreen : '#1AA928',
+        bgColorWhite : '#FFFFFF',
+        bgColorPage : '#C9CCCF',
+        bgColorLightGreen : '#E4FBE7',
+        bgColorEllipse : '#C4C4C4',
+        bgColorLanguageEllipse : '#D3F8D8',
+        bgColorSearchBar : '#ECF0F3',
+        // Font Colors
+        fontColorMenu : '#ABB5BA',
+        fontColorheading2 : '#303030',
+        fontColorHeading3 : '#333333',
+        fontColorNextBookAuthor : '#8A97A4',
+        fontColorNextBookDuration : "#8998A9",
+        fontColorPopularBookTitle : '#434C56',
+        fontColorPolularBookAuthor : '#8D96A8',
+        fontColorTopAuthor : '#434D56',
+        fontColorParagraph : '#DCE0E5',
+        fontColorUserName : '#212121',
+        fontColorPlayBookAuthor : '#70808F',
+        fontColorPlaceHolder : '#B2BAC8',
+        // StrokeColors
+        strokeColorGreen : '#27A857',
+        strokeColorLightGreen : '#D6F5E1'        
+    },
+
+    fontSizes : {
+        fsExtraLarge40 : '40px',
+        fsSemiLarge24 : '24px',
+        fsLarge22 : '22px',
+        fsLarge20 : '20px',
+        fsMedium18 : '18px',
+        fsSmall17 : '17px',
+        fsSemiSmall16 : '16px',
+        fsExtraSmall14 : '14px',
+    },
+
+    fontFamily :'Nunito',
+
+    fontWeights : {
+        regular : 400,
+        semiBold : 600,
+        bold : 700,
+        extraBold : 800
+    }
+}
