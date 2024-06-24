c01767fa2ab92d1c7dd6b255c2cb48188b1dc594
Ishara Madushani <isharam@alliontechnologies.com>
2024-06-24 14:16:06 +0530

    Correct book images inports

--- src/assests/pictures/books/index.ts
+++ src/assests/pictures/books/index.ts
@@ -1,20 +1,20 @@
 // Book desktop images
-import neverEatAloneBookDesktopImg from './books/desktop/book-never_eat_alone-desktop-img.jpg';
-import greatGatsbyBookDesktopImg from './books/desktop/book-the_great_gatsby-desktop-img.jpg';
-import sapiensBookDesktopImg from './books/desktop/book-sapiens-desktop-img.jpg';
-import theBookThiefBookDesktopImg from './books/desktop/book-the_book_theif-desktop-img.jpg';
-import sherlockHomesBookDesktopImg from './books/desktop/book-sherlock_homes-desktop-img.jpg';
-import faultInOurStarsBookDesktopImg from './books/desktop/book-fault_in_our_stars-desktop-img.jpg';
-import wonderBookDesktopImg from './books/desktop/book-wonder-desktop-img.jpg';
+import neverEatAloneBookDesktopImg from './desktop/book-never_eat_alone-desktop-img.jpg';
+import greatGatsbyBookDesktopImg from './desktop/book-the_great_gatsby-desktop-img.jpg';
+import sapiensBookDesktopImg from './desktop/book-sapiens-desktop-img.jpg';
+import theBookThiefBookDesktopImg from './desktop/book-the_book_theif-desktop-img.jpg';
+import sherlockHomesBookDesktopImg from './desktop/book-sherlock_homes-desktop-img.jpg';
+import faultInOurStarsBookDesktopImg from './desktop/book-fault_in_our_stars-desktop-img.jpg';
+import wonderBookDesktopImg from './/desktop/book-wonder-desktop-img.jpg';
 
 // Book mobile images
-import neverEatAloneBookMobileImg from './books/mobile/book-never_eat_alone-mobile-img.jpg';
-import greatGatsbyBookMobileImg from './books/mobile/book-the_great_gatsby-mobile-img.jpg';
-import sapiensBookMobileImg from './books/mobile/book-sapiens-mobile-img.jpg';
-import theBookThiefBookMobileImg from './books/mobile/book-the_book_theif-mobile-img.jpg';
-import sherlockHomesBookMobileImg from './books/mobile/book-sherlock_homes-mobile-img.jpg';
-import faultInOurStarsBookMobileImg from './books/mobile/book-fault_in_our_stars-mobile-img.jpg';
-import wonderBookMobileImg from './books/mobile/book-wonder-mobile-img.jpg';
+import neverEatAloneBookMobileImg from './mobile/book-never_eat_alone-mobile-img.jpg';
+import greatGatsbyBookMobileImg from './mobile/book-the_great_gatsby-mobile-img.jpg';
+import sapiensBookMobileImg from './mobile/book-sapiens-mobile-img.jpg';
+import theBookThiefBookMobileImg from './mobile/book-the_book_theif-mobile-img.jpg';
+import sherlockHomesBookMobileImg from './mobile/book-sherlock_homes-mobile-img.jpg';
+import faultInOurStarsBookMobileImg from './mobile/book-fault_in_our_stars-mobile-img.jpg';
+import wonderBookMobileImg from './mobile/book-wonder-mobile-img.jpg';
 
 export const BOOK_IMAGES = {
     DESKTOP : {
